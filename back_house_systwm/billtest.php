eab4ffd637ef6627da252543428932304f5ecec2
jirapat252262120 <q3.hahaha@gmail.com>
2021-03-10 16:16:47 +0700

    add

--- back_house_systwm/billtest.php
+++ back_house_systwm/billtest.php
@@ -1,8 +1,31 @@
 <?php 
         
-        echo"มา55555555555555555555";
         if(isset($_POST['submit']) == "submit"){
-            echo"มา";
+            $f_name = $_POST['f_name'];
+            $l_name = $_POST['l_name'];
+            $p_number = $_POST['p_number'];
+            $t_address = $_POST['t_address'];
+            $sl_sala = $_POST['sl_sala'];
+            $date_start = $_POST['date_start'];
+
+
+            $equip_foreach = $_POST['equip'];
+            $cabamlung_foreach = $_POST['cabamlung'];
+
+            $equip = foreach($equip_foreach as $key => $value){
+                echo $value, ;
+            }
+            $cabamlung = foreach($cabamlung_foreach as $key => $value){
+                echo $value, ;
+            }
+
+            echo " $equip";
+            echo " $cabamlung";
+
+
+
+
+
         }else{
             echo"ไม่มา";
         }
