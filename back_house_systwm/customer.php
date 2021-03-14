0e5c4326e4634e9b87a625076cfdd32f1c708208
jirapat252262120 <q3.hahaha@gmail.com>
2021-03-14 16:45:17 +0700

    go

--- back_house_systwm/customer.php
+++ back_house_systwm/customer.php
@@ -111,7 +111,7 @@
                             } 
                         ?>
                             <div class="d-flex justify-content-end">
-                            <b class="text-success">ราคาทั้งหมด : </b><input id="equip-total" type="number" disabled="true" class="text-right ml-2 font-weight-bold text-danger bg-dark" value="0"> <b class="text-success ml-2">บาท</b>
+                            <b class="text-success d-flex align-items-center">ราคาทั้งหมด : </b><input id="equip-total" type="number" disabled="true" class="text-right ml-2 font-weight-bold text-danger" value="0"> <b class="text-success ml-2 d-flex align-items-center">บาท</b>
                             </div>
                     </div> 
                     </div>
@@ -224,6 +224,10 @@
                                                                     }                      
                                                         ?>
                                                                     console.log(sum);
+                                                                    
+                                                                   
+                                                                      $("#equip-total").val("");
+                                                                    
                                                                     $("#equip-total").val(sum) ;
                                                                   
                                                                   
