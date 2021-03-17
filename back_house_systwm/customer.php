bfedc30720d5508b9c11a04a233fa0a70265a11f
flukza46 <thanakon.in@nsru.ac.th>
2021-03-17 15:28:41 +0700

    fluk

--- back_house_systwm/customer.php
+++ back_house_systwm/customer.php
@@ -17,25 +17,30 @@
 
               <form action="billtest.php" method="POST">
 
-
+                      <div class="row">
                       <div class="form-group col-4 mt-4">
                         <label for="exampleFormControlInput1"><i class="fas fa-user-edit text-primary"></i> ชื่อจริง</label>                        
                         <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="ชื่อจริง" name="f_name"> 
                       </div>
 
-                      <div class="form-group col-4"> 
+                      <div class="form-group col-4 mt-4"> 
                         <label for="exampleFormControlInput1">นามสกุล</label>
                         <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="นามสกุล" name="l_name">
                       </div>
+                      </div>
 
+                      <div class="row">
                       <div class="form-group col-4"> 
                         <label for="exampleFormControlInput1"><i class="fas fa-phone-volume text-info"></i> เบอร์โทรศัพท์</label>
                         <input  style="width:200px;" maxlength="10" type="text" class="form-control" id="exampleFormControlInput1"  name="p_number">
                       </div>
+                      </div>
 
+                      <div class="row">
                       <div class="form-group col-8">
                         <label for="exampleFormControlTextarea1"><i class="fas fa-map-marked-alt text-success"></i> สถานที่อยู่</label>
                         <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" name="t_address"></textarea>
+                      </div>
                       </div>
 
                     
