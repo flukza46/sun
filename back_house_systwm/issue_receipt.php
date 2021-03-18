168af46e05898acbdbbda0eae97ca22d19ad3b10
flukza46 <thanakon.in@nsru.ac.th>
2021-03-18 15:49:36 +0700

    flukkykykyyky

--- back_house_systwm/issue_receipt.php
+++ back_house_systwm/issue_receipt.php
@@ -1,1 +1,12 @@
-ออกใบเสร็จค่าไฟ และ ค่า manternance 9 คน
+
+<div class="card">
+    <div class="card-header">
+    ออกใบเสร็จค่าไฟ และ ค่า manternance 9 คน
+    
+
+    </div>
+    <div class="card-body">
+    
+    
+    </div>
+</div>
--- back_house_systwm/view_receipt.php
+++ back_house_systwm/view_receipt.php
@@ -1,1 +1,12 @@
-รองรับการดู
+
+<div class="card">
+    <div class="card-header">
+    รองรับการดู
+    
+
+    </div>
+    <div class="card-body">
+    
+    
+    </div>
+</div>
